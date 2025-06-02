1d8110a5a2c97cf73f5513cb687573c877156247
HuuBng <89332259+HuuBng@users.noreply.github.com>
2025-06-02 15:26:27 +0700

    Commit

---
+++ math-util/src/main/java/com/huubng/tool/mathutil/core/MathUtility.java
@@ -0,0 +1,19 @@
+package com.huubng.tool.mathutil.core;
+
+public class MathUtility {
+
+    // CLASS NÀY CHỨA CÁC HÀM STATIC LÀ NHỮNG HÀM TIỆN ÍCH DÙNG CHUNG CO MỌI NƠI
+
+
+    // ĐÂY LÀ HÀM MÔ PHỎNG THƯ VIỆN TOÁN HỌC MATH, CỦA JDK
+    // HÀM NÀY TÍNH N!
+    // N! = 1.2.3...N
+    // KO CÓ GIAI THỪA SỐ ÂM
+    // 0! = 1
+    // 20! VỪA ĐỦ KIỂU LONG, TỨC 21! VƯỢT KIỂU LONG
+    public static long getFactorial(int n) {
+
+        return 1;
+    }
+
+}
