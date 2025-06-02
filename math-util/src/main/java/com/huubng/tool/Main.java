c5421afc76fd0cf6b6e92e8cadf4a25f58bf7678
HuuBng <89332259+HuuBng@users.noreply.github.com>
2025-06-02 15:37:28 +0700

    Update Main.java

--- math-util/src/main/java/com/huubng/tool/Main.java
+++ math-util/src/main/java/com/huubng/tool/Main.java
@@ -5,6 +5,8 @@ import com.huubng.tool.mathutil.core.MathUtility;
 public class Main {
     public static void main(String[] args) {
         testFactorialGivenRightArgument0RunsWell();
+        testFactorialGivenRightArgument1RunsWell();
+        testFactorialGivenRightArgumentRunsWell();
     }
 
     // TEST THỬ HÀM VỪA GÕ
@@ -21,6 +23,29 @@ public class Main {
         long actual = MathUtility.getFactorial(n);
 
         System.out.println("0! expected: " + expected + " actual: " + actual);
+
+    }
+
+    // TEST CASE #2: CHECK/VERIFY THE GETFACTORIAL() METHOD WITH N = 1
+    // STEPS: GIVEN N = 1
+    //        GETFACTORIAL(N = 1)
+    // EXPECTED RESULT: HOPE TO SEE 1 AS THE RESULT OF 1!
+    // STATUS: ...
+
+    public static void testFactorialGivenRightArgument1RunsWell() {
+        int n = 1;
+        long expected = 1;
+        long actual = MathUtility.getFactorial(n);
+
+        System.out.println("1! expected: " + expected + " actual: " + actual);
+
+    }
+
+    public static void testFactorialGivenRightArgumentRunsWell() {
+        System.out.println("2! expected: 2" + " actual: " + MathUtility.getFactorial(2));
+        System.out.println("3! expected: 6" + " actual: " + MathUtility.getFactorial(3));
+        System.out.println("4! expected: 24" + " actual: " + MathUtility.getFactorial(4));
+        System.out.println("5! expected: 120" + " actual: " + MathUtility.getFactorial(5));
 
     }
 }
