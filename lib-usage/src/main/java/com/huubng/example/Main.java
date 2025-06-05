10bedba791e583074e5fd10d5ff9357791f75404
HuuBng <89332259+HuuBng@users.noreply.github.com>
2025-06-05 16:47:56 +0700

    commit

---
+++ lib-usage/src/main/java/com/huubng/example/Main.java
@@ -0,0 +1,12 @@
+package com.huubng.example;
+
+import com.huubng.tool.mathutil.core.MathUtility;
+
+public class Main {
+    public static void main(String[] args) {
+        // tui cần tính 6!, hy vọng ra 720
+        // có app nào làm đc ko
+        // thư viện 83.86 làm đc
+        System.out.println("6! = " + MathUtility.getFactorial(6));
+    }
+}
